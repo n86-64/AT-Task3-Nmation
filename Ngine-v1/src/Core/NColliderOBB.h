db11b606f3991dfc5151d9ab2545528f52a4e21f
Nathan <nathan.butt@gmail.com>
2018-10-30 12:16:48 +0000

    Modified OBB for collision testing
    MAJOR CHANGES:
    * Enhaned OBB to provide appropriate collision data for the
    calculations.
    
    * Added Basic function to perform OBB checks.

--- Ngine-v1/src/Core/NColliderOBB.h
+++ Ngine-v1/src/Core/NColliderOBB.h
@@ -10,11 +10,22 @@
 constexpr int NUMBER_OF_SEPERATING_AXES_PER_BOX = 9;
 constexpr int NUMBER_OF_POINTS = 8;
 
+constexpr int NUMBER_OF_AXES = 3; 
+
+
+class NPhysicsComponent;
+
 // Here we store the projection info for each normal.
 struct NColliderOBBProjection 
 {
 	float min;
 	float max;
+};
+
+struct NColliderOBBCollision 
+{
+	bool intersection;
+	NMath::Vector3 mtv; // Minimum translation vector. (Use for physics calculations.)
 };
 
 class NColliderOBB 
@@ -26,20 +37,34 @@ public:
 	void setPosition(NMath::Vector3  position);
 	void setSize(NMath::Vector3  newSize);
 
+	NColliderOBBCollision isObjectColliding(NPhysicsComponent* a, NPhysicsComponent* b);
 	// TODO - Add information to get position and direction.
 
 	// Get information to perform SAT check.
-	int				getaxesCount()  { return NUMBER_OF_SEPERATING_AXES_PER_BOX; }
-	int				getPointCount() { return NUMBER_OF_POINTS; }
-	NMath::Vector3  getPoint(int i) { return points[i]; }
-	NMath::Vector3  getAxis(int i)  { return axes[i]; }
 
 private:
-	NMath::Vector3   position;
-	NMath::Vector3   dimenstions;
-
-	// The number of points and sides.
-	NMath::Vector3   points[NUMBER_OF_POINTS]; // Positions in global space.
-	NMath::Vector3   axes[NUMBER_OF_SEPERATING_AXES_PER_BOX];  // The normalised axes to test against in global space.
+	NMath::Vector3   position; // The centre point of the box.
+	NMath::Vector3   dimenstions; // The dimenstions of the box (Defines the width of the box on each axis.)
+	NMath::Vector3   axes[NUMBER_OF_AXES];  // the axes values for the forward vectors.
+	
+#pragma region DEPRECATED_STUFF
 	NMath::Vector3	 edgeNormals[NUMBER_OF_SEPERATING_AXES_PER_BOX];
+	NMath::Vector3   points[NUMBER_OF_POINTS]; // Positions in global space.
+
+	// Unit points for the box. (May not be needed.)
+	NMath::Vector3   colliderLocalVeritcies[8]
+	{
+		{ NMath::Vector3(-1.0f,  1.0f, -1.0f) },
+		{ NMath::Vector3(1.0f,  1.0f, -1.0f) },
+		{ NMath::Vector3(1.0f,  1.0f,  1.0f) },
+		{ NMath::Vector3(-1.0f,  1.0f,  1.0f) },
+		{ NMath::Vector3(-1.0f, -1.0f, -1.0f) },
+		{ NMath::Vector3(1.0f, -1.0f, -1.0f)},
+		{ NMath::Vector3(1.0f, -1.0f,  1.0f) },
+		{ NMath::Vector3(-1.0f, -1.0f, 1.0f) },
+	};
+
+	void calculateEdges();
+#pragma endregion
+
 };
